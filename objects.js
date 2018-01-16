a2cb8232e27a5ea593e1c9358c09738d2ee1a1b3
Branden Mosley <branden.mosley1337@gmail.com>
2018-01-16 18:52:22 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,7 +4,7 @@ function updatePlaylist(obj, artistName, songTitle) {
   return Object.assign({}, obj, {[artistName]: songTitle})
 }
 
-function removeFromPlaylist(playlist, artistName) {
-  delete playlist.artistName
-  return playlist
+function removeFromPlaylist(obj, artistName) {
+  delete obj.artistName
+  return obj
 }
